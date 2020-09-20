74ea0861f78b443723d4c27f0a4ab3be6e9f65f5
Qasim Shaheen <qasimshaheen@outlook.com>
2020-09-20 06:51:45 -0700

    Added Extension Method for Generating Next Code based on previous code and an identifier

---
+++ SDMS API/ExtensionMethods/SDMSExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDMS_API.ExtensionMethods
+{
+    public static class SDMSExtensions
+    {
+
+        public static string GenerateNextCode(this string code, string identifier)
+        {
+            if (string.IsNullOrEmpty(code))
+                return "${identifier}-000001";
+
+            var newProductCode = Convert.ToInt32(code.Split(new char[] { '-' })[1]) + 1;
+
+            return $"{identifier}-{newProductCode.ToString().PadLeft(6, '0')}";
+        }
+    }
+}
