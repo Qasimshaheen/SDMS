9a7c4f8ffbbd88c1cd9db3cd7d893634f981e5e9
Qasim Shaheen <qasimshaheen@outlook.com>
2020-09-20 05:33:36 -0700

    Created Database And Initial User Table with EF Code First and setup it

---
+++ SDMS API/Data/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SDMS_API.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [StringLength(50)]
+        public string Username { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Password { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ SDMS API/Migrations/20200920122433_InitialMigrationWithUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SDMS_API.Migrations
+{
+    public partial class InitialMigrationWithUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(maxLength: 50, nullable: true),
+                    Name = table.Column<string>(maxLength: 50, nullable: true),
+                    Password = table.Column<string>(maxLength: 50, nullable: true),
+                    IsActive = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
