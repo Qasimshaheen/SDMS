61cdccd40cb2e160a7a506dc8b7c1de31b62ce64
Qasim Shaheen <qasimshaheen@outlook.com>
2020-09-29 10:04:28 -0700

    Update ChartOfAccount ViewModel
    Create ViewModels Properties in ChartofAccountEditVM

--- SDMS API/ViewModels/ChartOfAccount/ChartOfAccountCreateVM.cs
+++ SDMS API/ViewModels/ChartOfAccount/ChartOfAccountCreateVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,8 +8,11 @@ namespace SDMS_API.ViewModels.ChartOfAccount
 {
     public class ChartOfAccountCreateVM
     {
+        [StringLength(20)]
         public string AccountCode { get; set; }
+        [StringLength(50)]
         public string Name { get; set; }
+        [StringLength(50)]
         public string Category { get; set; }
         public bool IsDebit { get; set; }
         public int? ParentAccountId { get; set; }
--- SDMS API/ViewModels/ChartOfAccount/ChartOfAccountEditVM.cs
+++ SDMS API/ViewModels/ChartOfAccount/ChartOfAccountEditVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,16 @@ namespace SDMS_API.ViewModels.ChartOfAccount
 {
     public class ChartOfAccountEditVM
     {
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string AccountCode { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+        [StringLength(50)]
+        public string Category { get; set; }
+        public bool IsDebit { get; set; }
+        public int? ParentAccountId { get; set; }
+        public bool IsDetailAccount { get; set; }
+        public bool IsActive { get; set; }
     }
 }
