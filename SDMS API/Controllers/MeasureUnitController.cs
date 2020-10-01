64e4967d54f2047fb87bace06b861887d16b954f
Qasim Shaheen <qasimshaheen@outlook.com>
2020-10-01 09:33:11 -0700

    Added MeasureUnit Controller And ViewModel

---
+++ SDMS API/Controllers/MeasureUnitController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SDMS_API.Data;
+using SDMS_API.ViewModels.MesureUnit;
+
+namespace SDMS_API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MeasureUnitController : ControllerBase
+    {
+        private readonly SDMSDbContext _dbContext;
+
+        public MeasureUnitController(SDMSDbContext sDMSDbContext)
+        {
+            this._dbContext = sDMSDbContext;
+        }
+        [HttpGet]
+        public async Task<IEnumerable<MeasureUnitListingVM>> GetMeasureUnits()
+        {
+            var results = await _dbContext.MeasureUnits.Select(x => new MeasureUnitListingVM
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).ToListAsync();
+            return results;
+        }
+        [HttpGet]
+        public async Task<MeasureUnitDetailVM> GetMeasureUnitsById(int mesureUnitId)
+        {
+            var result = await _dbContext.MeasureUnits.Where(x => x.Id == mesureUnitId).Select(x => new MeasureUnitDetailVM
+            {
+                Id = x.Id,
+                Name = x.Name
+            }).FirstOrDefaultAsync();
+            return result;
+        }
+        [HttpDelete]
+        public async Task<bool> DeleteMeasureUnitById(int mesureUnitId)
+        {
+            var result = await _dbContext.MeasureUnits.Where(x => x.Id == mesureUnitId).FirstOrDefaultAsync();
+            if (result != null)
+            {
+                _dbContext.MeasureUnits.Remove(result);
+                var count = await _dbContext.SaveChangesAsync();
+                return count > 0;
+            }
+            return false;
+        }
+        [HttpPost]
+        public async Task<int> CreateMeasureUnit(MeasureUnitCreateVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var mesureUnit = new MeasureUnit()
+                {
+                    Name = model.Name
+                };
+                await _dbContext.MeasureUnits.AddAsync(mesureUnit);
+                await _dbContext.SaveChangesAsync();
+                return mesureUnit.Id;
+            }
+            else
+                return -1;
+        }
+        [HttpPut]
+        public async Task<int> EditMeasureUnt(MeasureUnitEditVM model)
+        {
+            var result = await _dbContext.MeasureUnits.Where(x => x.Id == model.Id).FirstOrDefaultAsync();
+            if (result != null)
+            {
+                result.Name = model.Name;
+                await _dbContext.SaveChangesAsync();
+                return result.Id;
+            }
+            else
+                return -1;
+        }
+
+    }
+}
---
+++ SDMS API/ViewModels/MesureUnit/MeasureUnitEditVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDMS_API.ViewModels.MesureUnit
+{
+    public class MeasureUnitEditVM
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Name { get; set; }
+    }
+}
