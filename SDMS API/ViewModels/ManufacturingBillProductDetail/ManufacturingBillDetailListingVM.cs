717f0d679edb1016475dd9f936e1d395201a5921
Qasimshaheen <qasimshaheen@outlook.com>
2020-11-01 03:24:10 -0800

    Added Bill Of Manufacturing Related ViewModels

---
+++ SDMS API/ViewModels/ManufacturingBillProductDetail/ManufacturingBillDetailListingVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SDMS_API.ViewModels.ManufacturingBillProductDetail
+{
+    public class ManufacturingBillDetailListingVM
+    {
+    }
+}
